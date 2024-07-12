eee300c05facf5c4bb0ad02609310d89124080ef
Savan <githost.2007@gmail.com>
2024-07-12 17:43:29 +0530

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -10,10 +10,10 @@ const Contact =()=>{
       <>
   <section className="bg-main bg-color contact-section hero-section text-white  d-flex flex-column justify-content-center align-items-center "> 
   <div className="mx-auto row-width">
-         <h1 className="common-text text-white">contact us</h1>
+         <h1 className="common-text text-white">book our service / contact us</h1>
            <hr className='w-50 mx-auto text-white'/>
          </div>
-               <div>
+         <div>
     <iframe
         src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3887.5763050857086!2d77.5897129!3d12.9989294!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bbe60ce1a0140d7%3A0x3ceab1e851eb586a!2sKarwar%2C%20Karnataka!5e0!3m2!1sen!2sin!4v1719659856903!5m2!1sen!2sin"
         width="100%"
@@ -27,58 +27,6 @@ const Contact =()=>{
           </div>
   <div className='container'>
              <form action="https://formspree.io/f/myzggwqv" method="post">
-
-      {/* <div className="mb-3">
-         <div className="row">
-          <div className="col-lg-6 col-12">
-          <label htmlFor="firstName" className="form-label">First Name</label>
-  <input className="form-control"                id="exampleFormUserFirstName"
-          type="text" 
-          name="First Name"
-          placeholder="First Name"
-          autoComplete="off"
-          required
-      />
-          </div>
-
-         <div className="col-lg-6 col-12">
-          <label htmlFor="lastName" className="form-label"> Last Name</label>
-  <input className="form-control" id="exampleFormUserLastName"
-          type="text" 
-          name="Last Name"
-          placeholder="Last Name"
-          autoComplete="off"
-          required
-      />
-          </div>
-                   <div className="col-lg-6 col-12">
-          <label htmlFor="lastName" className="form-label"> Last Name</label>
-  <input className="form-control" id="exampleFormUserLastName"
-          type="text" 
-          name="Last Name"
-          placeholder="Last Name"
-          autoComplete="off"
-          required
-      />
-          </div>
-         {/*</div>
-         </div>
-         <div className="mb-3">
-  <label htmlFor="email"className="form-label">Email address</label>
-  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"
-           name="Eamil"
-           autoComplete="off"
-           required
-      />
- </div>
-<div className="mb-3">
-  <label htmlFor="Textarea" className="form-label">Example textarea</label>
-  <textarea className="form-control"          id="exampleFormControlTextarea1"        rows="3"
-          name="Message"
-           autoComplete="off"
-           required
-      ></textarea>
-</div>*/}
           <div className="mb-3">
           <label htmlFor="firstName" className="form-label">First Name</label>
   <input className="form-control form-con"                id="exampleFormUserFirstName"
@@ -108,19 +56,30 @@ const Contact =()=>{
       />
  </div>
       <div className="mb-3">
-  <label htmlFor="Number"className="form-label">Mobile Number</label>
-  <input type="number" className="form-control" id="exampleFormControlInput1" placeholder="9364271352"
-           name="number"
+  <label htmlFor="number" className="form-label">Mobile Number</label>
+  <input 
+    type="tel" 
+    className="form-control" 
+    id="number" 
+    name="number" 
+    pattern="\d{10,}" 
+    placeholder="9364271352" 
+    autoComplete="off" 
+    required 
+  />
+</div>
+ <div className="mb-3">
+  <label htmlFor="Textarea" className="form-label"> Address (optional)</label>
+  <textarea className="form-control"          id="exampleFormControlTextarea1"        rows="3"
+          name="address"
            autoComplete="off"
-           required
-      />
- </div>
+      ></textarea>
+</div>
 <div className="mb-3">
-  <label htmlFor="Textarea" className="form-label">Example textarea</label>
+  <label htmlFor="Textarea" className="form-label"> Textarea (optional)</label>
   <textarea className="form-control"          id="exampleFormControlTextarea1"        rows="3"
           name="Message"
            autoComplete="off"
-           required
       ></textarea>
 </div>
 
